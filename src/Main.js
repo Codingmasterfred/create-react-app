92cb918e9dca96018584ebd8fa49e3af7f9aa8e9
Codingmasterfred <fredrickg777@gmail.com>
2023-04-20 10:43:33 -0700

    UPdated

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,34 @@
-import "./App.css"
 import HornedBeast from "./HornedBeast"
+import Gallery from "./data.json"
+import React from "react"
+import "./App.css"
 
-function Main(){
-return(
-    <div>
-        <main></main>
-        <HornedBeast Title ="Beast" imageUrl = "https://th.bing.com/th/id/OIP.9D3Cte5DamggZkAGacsYwwHaJ-?w=152&h=205&c=7&r=0&o=5&dpr=1.5&pid=1.7" text = "RUNN"/>
-        <HornedBeast Title ="Beast" imageUrl = "https://th.bing.com/th/id/OIP.SYeZtkWn3qW7oAQuOxSR6gHaFb?w=252&h=185&c=7&r=0&o=5&dpr=1.5&pid=1.7" text = "RUNN Fasterrr"/>
-    </div>
-);
+function Main() {
+    return (
+        <div class = "Div">
+            
+            <HornedBeast Title={Gallery[0].title} imageUrl={Gallery[0].image_url} text={Gallery[0].description} timeClicked={0} />
+            <HornedBeast Title={Gallery[1].title} imageUrl={Gallery[1].image_url} text={Gallery[1].description} timeClicked={0} />
+            <HornedBeast Title={Gallery[2].title} imageUrl={Gallery[2].image_url} text={Gallery[2].description} timeClicked={0} />
+            <HornedBeast Title={Gallery[3].title} imageUrl={Gallery[3].image_url} text={Gallery[3].description} timeClicked={0} />
+            <HornedBeast Title={Gallery[4].title} imageUrl={Gallery[4].image_url} text={Gallery[4].description} timeClicked={0} />
+            <HornedBeast Title={Gallery[5].title} imageUrl={Gallery[5].image_url} text={Gallery[5].description} timeClicked={0} />
+            <HornedBeast Title={Gallery[6].title} imageUrl={Gallery[6].image_url} text={Gallery[6].description} timeClicked={0} />
+            <HornedBeast Title={Gallery[7].title} imageUrl={Gallery[7].image_url} text={Gallery[7].description} timeClicked={0} />
+            <HornedBeast Title={Gallery[8].title} imageUrl={Gallery[8].image_url} text={Gallery[8].description} timeClicked={0} />
+            <HornedBeast Title={Gallery[9].title} imageUrl={Gallery[9].image_url} text={Gallery[9].description} timeClicked={0} />
+            <HornedBeast Title={Gallery[10].title} imageUrl={Gallery[10].image_url} text={Gallery[10].description} timeClicked={0} />
+            <HornedBeast Title={Gallery[11].title} imageUrl={Gallery[11].image_url} text={Gallery[11].description} timeClicked={0} />
+            <HornedBeast Title={Gallery[12].title} imageUrl={Gallery[12].image_url} text={Gallery[12].description} timeClicked={0} />
+            <HornedBeast Title={Gallery[13].title} imageUrl={Gallery[13].image_url} text={Gallery[0].description} timesClicked={0} />
+            <HornedBeast Title={Gallery[14].title} imageUrl={Gallery[14].image_url} text={Gallery[0].description} timesClicked={0} />
+            <HornedBeast Title={Gallery[15].title} imageUrl={Gallery[15].image_url} text={Gallery[0].description} timesClicked={0} />
+            <HornedBeast Title={Gallery[16].title} imageUrl={Gallery[16].image_url} text={Gallery[0].description} timesClicked={0} />
+            <HornedBeast Title={Gallery[17].title} imageUrl={Gallery[17].image_url} text={Gallery[0].description} timesClicked={0} />
+            <HornedBeast Title={Gallery[18].title} imageUrl={Gallery[18].image_url} text={Gallery[0].description} timesClicked={0} />
+            <HornedBeast Title={Gallery[19].title} imageUrl={Gallery[19].image_url} text={Gallery[0].description} timesClicked={0} />
+        </div>
+    );
 
 };
 
---
+++ src/README.md
@@ -0,0 +1,19 @@
+Number and name of feature: _Feature #1: Display Images_______________________________
+
+Estimate of time needed to complete: _1 hour____
+
+Start time: __11:23___
+
+Finish time: _12:16____
+
+Actual time needed to complete: __29 mins___
+
+Number and name of feature: ____________Feature #2: Allow Users to Favorite Individual Beasts____________________
+
+Estimate of time needed to complete: _____
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
