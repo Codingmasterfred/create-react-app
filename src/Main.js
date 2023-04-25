2685c3d73087c3ee104787ab599ca19c4d4a8f5e
Codingmasterfred <fredrickg777@gmail.com>
2023-04-25 06:39:49 -0700

    Class03 Lab

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,42 @@
 import HornedBeast from "./HornedBeast"
-import Gallery from "./data.json"
-import React from "react"
-import "./App.css"
+// This imports the "HornedBeast" component from a file called "HornedBeast.js" in the same directory as this file.
 
-function Main() {
+import React from "react"
+// This imports the React library from the node_modules directory.
+
+
+
+function Main({data, updateFunction, beastUpdateFunction}) {
+    // This declares the "Main" function component that takes in three props: "data", "updateFunction", and "beastUpdateFunction".
     return (
-        <div class = "Div">
+        <div className = "Div">
+            {/* This creates a div element with a class name of "Div" */}
+            {data.map((Beast,index) => (
+                //  This maps over each object in the "data" array and creates a new "HornedBeast" component for each one.
+                <HornedBeast
+                // This is the beginning of the "HornedBeast" component.
+                 key={index}
+                //  This sets the "key" prop to the current index of the mapped element
+                title={Beast.title}
+                // This sets the "title" prop to the title of the current element.
+                text={Beast.description}
+                // This sets the "text" prop to the description of the current element.
+                imageUrl={Beast.image_url}
+                // This sets the "imageUrl" prop to the image URL of the current element.
+                timeClicked={0}
+                // This sets the "timeClicked" prop to 0.
+                updateFunction={updateFunction}
+                //  This sets the "updateFunction" prop to the "updateFunction" prop that was passed to the "Main" component.
+                beastUpdateFunction={beastUpdateFunction}
+                // This sets the "beastUpdateFunction" prop to the "beastUpdateFunction" prop that was passed to the "Main" component.
+                >
+                    {/*  This closes the opening "HornedBeast" tag. */}
+                </HornedBeast>
+                //  This is the closing "HornedBeast" tag.
+
+            ))}
             
-            <HornedBeast Title={Gallery[0].title} imageUrl={Gallery[0].image_url} text={Gallery[0].description} timeClicked={0} />
+            {/* <HornedBeast Title={Gallery[0].title} imageUrl={Gallery[0].image_url} text={Gallery[0].description} timeClicked={0} />
             <HornedBeast Title={Gallery[1].title} imageUrl={Gallery[1].image_url} text={Gallery[1].description} timeClicked={0} />
             <HornedBeast Title={Gallery[2].title} imageUrl={Gallery[2].image_url} text={Gallery[2].description} timeClicked={0} />
             <HornedBeast Title={Gallery[3].title} imageUrl={Gallery[3].image_url} text={Gallery[3].description} timeClicked={0} />
@@ -26,7 +55,7 @@ function Main() {
             <HornedBeast Title={Gallery[16].title} imageUrl={Gallery[16].image_url} text={Gallery[0].description} timesClicked={0} />
             <HornedBeast Title={Gallery[17].title} imageUrl={Gallery[17].image_url} text={Gallery[0].description} timesClicked={0} />
             <HornedBeast Title={Gallery[18].title} imageUrl={Gallery[18].image_url} text={Gallery[0].description} timesClicked={0} />
-            <HornedBeast Title={Gallery[19].title} imageUrl={Gallery[19].image_url} text={Gallery[0].description} timesClicked={0} />
+            <HornedBeast Title={Gallery[19].title} imageUrl={Gallery[19].image_url} text={Gallery[0].description} timesClicked={0} /> */}
         </div>
     );
 
---
+++ src/SelectedBeast.js
@@ -0,0 +1,39 @@
+import {useState} from "react";
+//This line imports the useState hook from the React library, which is used to manage state in functional components.
+import React from "react"
+//This line imports the main React library, which is required in all React components.
+import Modal from "react-bootstrap/Modal"
+//This line imports the Modal component from the React-Bootstrap library, which is a pre-built component for creating modals in React.
+import Button from "react-bootstrap/Button"
+//This line imports the Button component from the React-Bootstrap library, which is a pre-built component for creating buttons in React.
+function SelectedBeast(props){
+//This is a functional component named SelectedBeast that takes props as its argument.
+ const handleClose = () => props.SetDisplaySelectedBeast(false);
+// This line creates a constant named handleClose, which is a function that sets the displaySelectedBeast state to false when called.
+
+return ( 
+<Modal id="Modal" show={true} onHide={handleClose}>
+{/* This is a Modal component that is displayed when show is set to true, and handleClose is called when the close button is clicked. */}
+    <Modal.Header id="ModalHeader" closeButton>
+    {/* This is the header section of the Modal, which contains a title and a close button. */}
+      <Modal.Title>{props.beast.title}</Modal.Title>
+      {/* This line displays the title of the beast passed in as a prop. */}
+    </Modal.Header>
+    <Modal.Body id="ModalImgDiv"><img id="ModalImg" src ={props.beast.imageUrl}/>{props.beast.text}</Modal.Body>
+    {/* This line displays the imageUrl and text of the beast passed in as a prop. */}
+    <Modal.Footer>
+        {/* This is the footer section of the Modal, which contains buttons. */}
+      <Button variant="secondary" onClick={handleClose}>
+        Close
+      </Button>
+      {/*  This line creates a secondary button with the text "Close" that calls handleClose when clicked. */}
+      <Button variant="primary" onClick={handleClose}>
+        Save Changes
+      </Button>
+      {/* This line creates a primary button with the text "Save Changes" that calls handleClose when clicked. */}
+    </Modal.Footer>
+  </Modal>)
+}
+
+export default SelectedBeast
+// This line exports the SelectedBeast component as the default export of this module, so that it can be imported and used in other modules.
